89599b65d1e61a04dfbac27c6082d8ca74cc096d
manjuchavva135 <manjureddychavva@gmail.com>
2022-06-30 10:09:34 +0200

    Generating training data

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from tensorflow import keras
 import pandas as pd
 import seaborn as sns
-from pylab import rcParams
+from matplotlib import rcParams
 import matplotlib.pyplot as plt
 from matplotlib import rc
 sns.set(style='whitegrid', palette='muted', font_scale=1.5)
@@ -11,3 +11,5 @@ rcParams['figure.figsize'] = 16, 10
 RANDOM_SEED = 42
 np.random.seed(RANDOM_SEED)
 tf.random.set_seed(RANDOM_SEED)
+time = np.arange(0, 100, 0.1)
+sin = np.sin(time) + np.random.normal(scale=0.5, size=len(time))
