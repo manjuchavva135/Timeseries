230fafd371d7c21966201359bd8aa04ad233452b
manjuchavva135 <manjureddychavva@gmail.com>
2022-06-30 10:10:42 +0200

    Data Preprocessing

--- main.py
+++ main.py
@@ -13,3 +13,8 @@ np.random.seed(RANDOM_SEED)
 tf.random.set_seed(RANDOM_SEED)
 time = np.arange(0, 100, 0.1)
 sin = np.sin(time) + np.random.normal(scale=0.5, size=len(time))
+df = pd.DataFrame(dict(sine=sin), index=time, columns=['sine'])
+train_size = int(len(df) * 0.8)
+test_size = len(df) - train_size
+train, test = df.iloc[0:train_size], df.iloc[train_size:len(df)]
+print(len(train), len(test))
